880883270071e45e105c714fba452efb890c37e4
NiklasKylli <Niklas.kylli@gmail.com>
2018-09-05 13:56:39 +0300

    Helloworld project intialized

---
+++ hello-worldd/hello-worldd/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hello_worldd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Niklas Kylli");
+        }
+    }
+}
